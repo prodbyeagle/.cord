fe3acf0b416ca4eb0df754c3c4d2c73a5d5c8b8e
prodbyeagle <124641014+prodbyeagle@users.noreply.github.com>
2025-08-04 19:57:02 +0200

    fix plugins broken by latest Discord update (#3574)

--- src/plugins/favGifSearch/index.tsx
+++ src/plugins/favGifSearch/index.tsx
@@ -23,7 +23,7 @@ interface SearchBarComponentProps {
 }
 
 type TSearchBarComponent =
-    React.FC<SearchBarComponentProps> & { Sizes: Record<"SMALL" | "MEDIUM" | "LARGE", string>; };
+    React.FC<SearchBarComponentProps>;
 
 interface Gif {
     format: number;
@@ -170,7 +170,7 @@ function SearchBar({ instance, SearchBarComponent }: { instance: Instance; Searc
             ref={ref}
             autoFocus={true}
             className={containerClasses.searchBar}
-            size={SearchBarComponent.Sizes.MEDIUM}
+            size="md"
             onChange={onChange}
             onClear={() => {
                 setQuery("");
